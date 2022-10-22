556da0acadf51ee353bc3cd85286dea1768a6eb5
anasilva81 <anasilva81@users.noreply.github.com>
2022-10-22 18:34:26 +0100

    Set current date

--- src/Current.js
+++ src/Current.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
+import CurrentDate from "./CurrentDate";
 import "./Current.css";
 import SearchBar from "./SearchBar";
 import axios from "axios";
 
-export default function Current() {
-  const [loaded, setLoaded] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
+export default function Current(props) {
+  const [weatherData, setWeatherData] = useState({ loaded: false });
 
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
+      loaded: true,
       city: response.data.city,
-      date: "Tuesday, 4 October",
+      date: new Date(response.data.time * 1000),
       hour: "16:03",
       currTemperature: response.data.temperature.current,
       humidity: response.data.temperature.humidity,
@@ -21,19 +22,19 @@ export default function Current() {
       //temperatureMin: response.data.daily[0].temp.min,
       //temperatureMax: response.data.daily[0].temp.max,
     });
-
-    setLoaded(true);
   }
 
-  if (loaded) {
+  if (weatherData.loaded) {
     return (
       <div className="container">
         <div className="wrap-curr-info">
           <div className="row justify-content-between">
             <div className="col-6">
-              <h1 id="city">{weatherData.city}</h1>
-              <h3 id="date">{weatherData.date}</h3>
-              <h3 id="hour">{weatherData.hour}</h3>
+              <h1>{weatherData.city}</h1>
+              <h3>
+                <CurrentDate date={weatherData.date} />
+              </h3>
+              <h3>{weatherData.hour}</h3>
             </div>
             <div className="col-6">
               <SearchBar />
@@ -110,8 +111,8 @@ export default function Current() {
   } else {
     const apiKey = "91b1f0t782317c69da4ae1170bo049f3";
     let units = "metric";
-    let city = "Porto";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=${units}`;
 
     axios.get(apiUrl).then(handleResponse);
 
---
+++ src/CurrentDate.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function CurrentDate(props) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[props.date.getDay()];
+  let date = props.date.getDate();
+  let month = months[props.date.getMonth()];
+
+  return (
+    <div>
+      {day}, {date} {month}
+    </div>
+  );
+}
